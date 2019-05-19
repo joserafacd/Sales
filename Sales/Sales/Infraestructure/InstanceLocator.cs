9ab892b43c54c25fc6e7b09e79b24ffb73ec670a
Jose Carcamo <joserafacd@hotmail.com>
2019-05-19 12:29:51 -0600

    Configuracion de ConecctionString y Video 7

---
+++ Sales/Sales/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,16 @@
+﻿using Sales.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales.Infraestructure
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Sales.ViewModels
+{
+    using Sales.Common.Models;
+    using Sales.Services;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Xamarin.Forms;
+
+    public class ProductsViewModel: BaseViewModel
+    {
+        private ApiServices apiService;
+        public ObservableCollection<Product> products;
+        public ObservableCollection<Product> Products {
+            get { return this.products; }
+            set { this.SetValue(ref this.products, value); }
+        }
+
+        public ProductsViewModel()
+        {
+            this.apiService = new ApiServices();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            var response = await apiService.GetList<Product>("aqui va localhost:puerto", "/api", "/Products");
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                return;
+            }
+
+            var list = (List<Product>)response.Result;
+            this.Products = new ObservableCollection<Product>(list);
+        }
+    }
+}
